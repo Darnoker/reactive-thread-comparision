0c3abd06a548182be87d9c7dc45988694de8a5ef
Konrad Głąb <Darnok10000@gmail.com>
2024-12-01 14:33:48 +0100

    initial commit

---
+++ src/main/java/pl/edu/ug/konrad/glab/reactive_thread_compare/ReactiveThreadComparisionApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.konrad.glab.reactive_thread_compare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveThreadComparisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveThreadComparisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/ug/konrad/glab/reactive_thread_compare/ReactiveThreadComparisionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.konrad.glab.reactive_thread_compare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveThreadComparisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
